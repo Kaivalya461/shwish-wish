af0c6286ff25ea5e92575472889ff82d3a4e079b
kaivalya461 <kaivalyathanekar@gmail.com>
2025-03-22 16:16:46 +0530

    Add Angular Material theme support and dummy context. Reverted the baseHref back to '/'

--- ui/express-server/index.js
+++ ui/express-server/index.js
@@ -4,15 +4,15 @@ const path = require('path');
 const app = express();
 
 // Serve static files from the Angular build folder
-app.use('/sh', express.static(path.join(__dirname, 'shwish-wish-app/dist/shwish-wish-app/browser')));
+app.use('/', express.static(path.join(__dirname, 'shwish-wish-app/dist/shwish-wish-app/browser')));
 
 // Redirect all requests to Angular's index.html
-app.get('/sh/*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'shwish-wish-app/dist/shwish-wish-app/browser/index.html'));
 });
 
 // Start the server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}/sh`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
